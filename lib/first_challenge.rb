496506fee2c91cf4cf6942ab99b9b7c51782c7af
w.h.sinn <w.h.sinn@gmail.com>
2019-03-23 02:36:25 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,13 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+contacts["Freddy Mercury"].each do |attribute, value|
+  if attribute == :favorite_icecream_flavors
+    value.delete_if do |flavor|
+      flavor == "strawberry"
+    end
+  end
+end
   contacts
 end
 
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,15 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  #code your solution here!
-
   
-
+ groceries.values.recursive_flatten(array, results = [])
+  array.each do |element|
+    if element.class == Array
+      recursive_flatten(element, results)
+    else
+      results << element
+    end
+  end
+  results
+end
 end
